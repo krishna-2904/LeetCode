e6523d32912fab8535798eb1701333138e15075a
krishna-2904 <krishnaguptakg2904@gmail.com>
2026-09-24 23:23:03 +0530

    Time: 1940 ms (5.15%) | Memory: 42.2 MB (50.22%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long l = 0;
+        long r = 10000000;
+        long n = 1;
+        while(l<=r){
+            n = l*l + r*r;
+            if(n==c)
+                return true;
+            if(n>c)
+                r--;
+            else
+                l++;
+        }
+        return false;
+    }
+}
